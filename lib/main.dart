56a7f30a864c79d1da50ca3df955f98ad838644d
bercam <rafael.bercam@maxmilhas.com.br>
2020-04-08 09:07:40 -0300

    fisrt commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bercambank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.green[800],
+          accentColor: Colors.blueAccent[700],
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary
+          )
+      ),
+      home: ListaTransferencias(),
+    );
+  }
+}
+
